6f5b32fae9fb626d294457de54cd85bbd10b0909
flystary <651360107@qq.com>
2021-09-08 13:21:29 +0800

    Algorithm

---
+++ Concurrent/log/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	//log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
+	//log.Println("这是一条普通的日志！")
+
+	//logfile, err := os.OpenFile("./xx.log",os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	//if err != nil {
+	//	fmt.Println("open log file failed, err:", err)
+	//	return
+	//}
+	//log.SetOutput(logfile)
+	//log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
+
+}
